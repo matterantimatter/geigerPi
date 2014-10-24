6e86ac002b9063c5b390b863a845a686416ec5d6
Matt Howard <matterantimatter@gmail.com>
2014-10-24 13:13:27 -0400

    inital build of python data collector.  I need to look at pythons logging facilties to capture millisecond level events

---
+++ clickCheck.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+import time
+import RPi.GPIO as gpio
+
+gpio.VERSION
+gpio.setmode(gpio.BOARD)
+gpio.setup(7, gpio.IN)
+
+timeStart = time.clock()
+lastClick = timeStart
+clickTotal = 0
+maxTime=0
+minTime = 1
+avgTime = 0
+elapseSum = 0
+
+while (1):
+	clickState = gpio.input(7)
+	
+	if clickState == 1:
+		clickTime = time.clock()
+		timeElapsed = clickTime - lastClick 
+		lastClick = clickTime
+		clickTotal+=1
+		if timeElapsed > maxTime:
+			maxTime = timeElapsed
+	
+		if timeElapsed < minTime:
+			minTime = timeElapsed
+
+		avgTime = (elapseSum + timeElapsed) / clickTotal
+		elapseSum += timeElapsed
+
+		#print( "{} - Click detected".format( time.asctime() ))
+		print( "click detected: ",clickTime, timeElapsed, minTime, maxTime, avgTime, clickTotal)
